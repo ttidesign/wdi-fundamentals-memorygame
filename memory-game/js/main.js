b0481ba79d6164d7e1140d0f011b388f18e2fccc
TUONG NGUYEN <tknguyen@TUONGs-MacBook-Pro.local>
2020-03-05 22:59:54 -0800

    createdgame logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,14 +1,22 @@
-console.log("up and running")
+
 let cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-console.log(cardsInPlay);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match")}
- else { alert("Sorry try again")}}
+function checkForMatch() { 
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+ 	console.log('You found a match!');
+ } else {
+ 	console.log('Sorry, try again.');
+ }
+}
+function flipCard(cardID) { 
+
+ console.log('User flipped '+ cards[cardID]);
+ cardsInPlay.push(cards[cardID])
+ checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
 
