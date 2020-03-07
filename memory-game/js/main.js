9a9e2990ddf2ac20e02447c5641dd5cb2ffebdf4
TUONG NGUYEN <tknguyen@TUONGs-MacBook-Pro.local>
2020-03-06 16:02:58 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,17 +1,49 @@
 
-let cards = ['queen', 'queen', 'king', 'king'];
+let cards = [
+{
+  	rank: "queen",
+  	suit: "hearts",
+  	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png" 
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
+
+
+
+
+
+
+
 let cardsInPlay = [];
 function checkForMatch() { 
  if (cardsInPlay[0] === cardsInPlay[1]) {
  	console.log('You found a match!');
  } else {
- 	console.log('Sorry, try again.');
+ 	alert('Sorry, try again.');
  }
 }
 function flipCard(cardID) { 
 
- console.log('User flipped '+ cards[cardID]);
- cardsInPlay.push(cards[cardID])
+ console.log('User flipped '+ cards[cardID].rank);
+ cardsInPlay.push(cards[cardID].rank);
+ console.log(cards[cardID].cardImage);
+ console.log(cards[cardID].suit);
  checkForMatch();
 };
 
